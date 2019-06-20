0e7397f6da7235a5dec5bf0bdea5d917fdc227b3
khkr <khkrlife@gmail.com>
2019-06-20 15:17:17 +0530

    arithmetic operators

---
+++ arithmeticOperators.py
@@ -0,0 +1,38 @@
+a = 5
+
+b = a +5
+
+print(b)
+
+b = a -2
+
+print(b)
+
+b = a*3
+
+print(b)
+
+b = a**4
+
+print(b)
+
+# Implicit Conversion
+b = a/2
+
+#Explicit Conversion
+c = a/2.0
+
+print(c)
+
+b = a%2
+
+print(b)
+
+
+
+
+
+
+
+
+
