809dab456e6b481207971e617a905e8900da46e3
khkr <khkrlife@gmail.com>
2019-06-28 11:45:28 +0530

    String methods

---
+++ StringMethods.py
@@ -0,0 +1,71 @@
+
+# String  can store : Sentences, Paragraphs, words 
+# AS long as something is under " " , Strings can store them
+
+firstString = "Hello World"
+
+# print(firstString)
+
+# He exclaimed: "You are amazing" 
+
+secondString = "He exclaimed: \"You are amazing\" "
+
+# print(secondString)
+
+# He exclaimed: "You're amazing "
+
+thirdString = 'He exclaimed: "You\'re amazing" '
+
+# print(thirdString)
+
+# We are going to take a look at few string methods
+
+name = "vikram aditya"
+
+# print(name.title())
+
+hello = "Hello"
+
+# print(len(name))
+
+# print(name)
+
+# print(name.islower())
+
+# print(hello)
+
+# print(hello.islower())
+
+# s = "Almond Oil, Coconut Oil, Peanut Oil, Sunflower Oil, Olive Oil"
+
+
+# print(s.count("Oil"))
+
+
+a = "Hi "
+
+b= "How are you doing ?"
+
+
+print(a+" "+b)
+
+print(a*10)
+
+print(a/10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ quotes.py
@@ -0,0 +1,4 @@
+# TODO: Fix this string!
+ford_quote = 'Whether you think you can, or you think you can't--you're right.'
+
+print(ford_quote)
