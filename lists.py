7119897f6b16f3f27d8b238f04ab9372b3e71f6a
khkr <khkrlife@gmail.com>
2019-07-01 11:18:52 +0530

    Lists basics

---
+++ lists.py
@@ -0,0 +1,31 @@
+days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+
+
+# 0 is the first index
+print("The first day of the week is: ",days[0])
+
+# It has array out of bounds exception 
+# So I cannot access days[10]
+
+#We can access elements using negative index
+
+print("The last day of the week is: ", days[-1])
+
+# We want to the print the first 3 days
+
+print("The first three days of the week are: ",days[:3])
+
+print("The last four days of the week are: ",days[-4:])
+
+
+
+
+
+
+
+
+
+
+
+
+
