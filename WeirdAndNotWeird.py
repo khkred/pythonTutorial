78f77cbed3d314cba7c0e0d676c582188018c0d0
khkr <khkrlife@gmail.com>
2019-07-09 11:49:33 +0530

    hacker rank python 1st and 2nd challenges

---
+++ WeirdAndNotWeird.py
@@ -0,0 +1,30 @@
+# get input n
+
+n = int(input())
+
+# 1<=n<=100
+
+if 1<=n and n<=100:
+	
+	# We are checking if n is odd
+	if n%2==1:
+		print("Weird")
+	# Else condition means that n is even
+	else:
+		if 2<=n and n<=5:
+			print("Not Weird")
+
+		#Next if condition
+		# if n is between 6 to 20 "Weird"
+		if 6<=n and n<=20:
+			print("Weird")
+
+		#if n > 20 . Print Not Weird
+		if n>20:
+			print("Not Weird")
+
+
+
+
+
+
