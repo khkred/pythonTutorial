2174ccbbcba201aae89a8909cb867edcc60b0553
khkr <khkrlife@gmail.com>
2019-07-11 11:52:46 +0530

    for loop in dictionaries using seinfield cast members

--- seinfield-cast.py
+++ seinfield-cast.py
@@ -5,6 +5,25 @@ cast = {
 "Michael Richards": "Cosmo Kramer"
 }
 
-
+# Here we are printing key values
 for key in cast:
 	print(key)
+
+# Now we want to print the pair values
+
+for key,pair in cast.items():
+
+	print(pair)
+
+# We want to print both key and pair values
+
+for key,pair in cast.items():
+
+	print("Actor: ",key," Role: ",pair)
+
+
+
+
+
+
+
