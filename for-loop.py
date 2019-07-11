3a952b1fd53e986bef6a393bb164fa74fce60371
khkr <khkrlife@gmail.com>
2019-07-11 11:48:25 +0530

    for -loops

---
+++ for-loop.py
@@ -0,0 +1,45 @@
+# if we want to print 20 numbers
+
+# for i in range(20):
+
+# 	print(i+1)
+
+
+
+# print numbers below 20 with a difference of 3
+
+# for i in range(1,20,3):
+#  	print(i)
+
+# We want to capitalise the city names
+
+cities = ['new york city', 'mountain view', 'chicago', 'los angeles']
+
+capitalised_cities = []
+
+# in this for loop we are capitalising every city
+# and then we area adding it to the capitalised_cities list
+
+for city in cities:
+
+	capitalised_cities.append(city.title())
+
+# Now we will just print out the capitalised cities
+
+for city in capitalised_cities:
+	print(city)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
